6074ecb1902c64a73c1146bb771480a43a06f4e6
LynnHo <lynn_ho@163.com>
2020-01-03 23:20:53 +0800

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [CelebA](http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html) dataset provides an aligned set `img_align_celeba.zip`. However, the size of each aligned image is **218x178**, so the faces cropped from such images would be even smaller!
 
-Here we provide a code to obtain **higher resolution** face images, by cropping the faces from the original unaligned images via 5 landmarks.
+Here we provide a code to obtain **higher resolution** face images, by cropping the faces from the original unaligned images via 68 landmarks.
 
 ***We also use a [deep image quality assessment](https://github.com/dmaniry/deepIQA) method to evaluate and rank the cropped image quality in [scores.txt](scores.txt), lower score the better.***
 
@@ -18,66 +18,68 @@ Here we provide a code to obtain **higher resolution** face images, by cropping 
 ## Usage
 
 - Prerequisites
-    - OpenCV 3 (much faster) or scikit-image
-    - Python 2 or 3
+
+    - OpenCV
+
+    - Python 3.6
 
 - Dataset
-    - download the dataset
-        - **img_celeba.7z**
-            - [Baidu Netdisk](https://pan.baidu.com/s/1eSNpdRG#list/path=%2Fsharelink2785600790-938296576863897%2FCelebA%2FImg&parentPath=%2Fsharelink2785600790-938296576863897) or
-            - [Google Drive](https://drive.google.com/drive/folders/0B7EVK8r0v71pTUZsaXdaSnZBZzg)
-        - **list_landmarks_celeba.txt**
-            - [Baidu Netdisk](https://pan.baidu.com/s/1eSNpdRG#list/path=%2Fsharelink2785600790-938296576863897%2FCelebA%2FAnno&parentPath=%2Fsharelink2785600790-938296576863897) or
-            - [Google Drive](https://drive.google.com/drive/folders/0B7EVK8r0v71pOC0wOVZlQnFfaGs)
-    - unzip the data
+
+    - [CelebA](http://openaccess.thecvf.com/content_iccv_2015/papers/Liu_Deep_Learning_Face_ICCV_2015_paper.pdf)-unaligned (10.2GB, higher quality than the aligned data)
+
+        - download the dataset
+
+            - img_celeba.7z (move to **./data/img_celeba.7z**): [Google Drive](https://drive.google.com/drive/folders/0B7EVK8r0v71pTUZsaXdaSnZBZzg) or [Baidu Netdisk](https://pan.baidu.com/s/1eSNpdRG#list/path=%2Fsharelink2785600790-938296576863897%2FCelebA%2FImg&parentPath=%2Fsharelink2785600790-938296576863897)
+
+            - annotations.zip (move to **./data/annotations.zip**): [Google Drive](https://drive.google.com/file/d/1xd-d1WRnbt3yJnwh5ORGZI3g-YS-fKM9/view?usp=sharing)
+
+        - unzip the data
+
+            ```console
+            7z x ./data/img_celeba.7z/img_celeba.7z.001 -o./data/
+
+            unzip ./data/annotations.zip -d ./data/
+            ```
+
+- Cropping Examples
+
+    - 512x512 + lanczos4 + jpg
 
         ```console
-        7z x img_celeba.7z.001
+        python align.py --crop_size_h 512 --crop_size_w 512 --order 4 --save_format jpg --n_worker 32
         ```
 
-    - organize the files as follow
-
-        ```
-        path_to_dataset
-        ├── data/*.jpg
-        └── list_landmarks_celeba.txt
-        ```
-
-- Examples
-
-    - 512x512 + lanczos4 (with OpenCV) + jpg
-
-        ```console
-        python hd_celeba.py --data_dir path_to_dataset --crop_size 512 --order 4 --save_format jpg --n_worker 32
-        ```
         <p align="center"> <img src="./pics/512_lanczos4.jpg" width="49.5%"> </p>
 
-    - 512x512 + lanczos4 (with OpenCV) + png + larger face in the image (by setting `face_factor`, default is 0.7)
+    - 512x512 + lanczos4 + png + larger face in the image (by setting `face_factor`, default is 0.45)
 
         ```console
-        python hd_celeba.py --data_dir path_to_dataset --crop_size 512 --order 4 --save_format png --face_factor 0.8 --n_worker 32
+        python align.py --crop_size_h 512 --crop_size_w 512 --order 4 --save_format png --face_factor 0.6 --n_worker 32
         ```
-        <p align="center">  <img src="./pics/512_lanczos4_0.8.png" width="49.5%"> </p>
 
-    - 384x384 + bicubic + jpg + smaller face in the image (by setting `face_factor`, default is 0.7)
+        <p align="center">  <img src="./pics/512_lanczos4_0.6.png" width="49.5%"> </p>
+
+    - 384x384 + bicubic + jpg + smaller face in the image (by setting `face_factor`, default is 0.45)
 
         ```console
-        python hd_celeba.py --data_dir path_to_dataset --crop_size 384 --order 3 --save_format jpg --face_factor 0.65 --n_worker 32
+        python align.py --crop_size_h 384 --crop_size_w 384 --order 3 --save_format jpg --face_factor 0.3 --n_worker 32
         ```
+
         <p align="center">  <img src="./pics/384_bicubic.jpg" width="37.125%"> </p>
 
 - Notice
-    - `order` for OpenCV
+
+    - `order`
+
         - 0: INTER_NEAREST
+
         - 1: INTER_LINEAR
+
         - 2: INTER_AREA
+
         - 3: INTER_CUBIC
+
         - 4: INTER_LANCZOS4
+
         - 5: INTER_LANCZOS4
-    - `order` for scikit-image
-        - 0: Nearest-neighbor
-        - 1: Bi-linear
-        - 2: Bi-quadratic
-        - 3: Bi-cubic
-        - 4: Bi-quartic
-        - 5: Bi-quintic
+
--- cropper.py
+++ cropper.py
@@ -1,51 +1,34 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import numpy as np
 
 
-_DEFAULT_MEAN_LANDMARKS = np.array([[-0.46911814, -0.51348481],
-                                    [0.45750203, -0.53173911],
-                                    [-0.00499168, 0.06126145],
-                                    [-0.40616926, 0.46826089],
-                                    [0.42776873, 0.45444013]])
-
-
-def align_crop_5pts_opencv(img,
-                           src_landmarks,
-                           mean_landmarks=_DEFAULT_MEAN_LANDMARKS,
-                           crop_size=512,
-                           face_factor=0.7,
-                           landmark_factor=0.35,
-                           align_type='similarity',
-                           order=3,
-                           mode='edge'):
-    """Align and crop a face image by 5 landmarks.
+def align_crop_opencv(img,
+                      src_landmarks,
+                      standard_landmarks,
+                      crop_size=512,
+                      face_factor=0.7,
+                      align_type='similarity',
+                      order=3,
+                      mode='edge'):
+    """Align and crop a face image by landmarks.
 
     Arguments:
-        img             : Face image to be aligned and cropped.
-        src_landmarks   : 5 landmarks:
-                              [[left_eye_x, left_eye_y],
-                               [right_eye_x, right_eye_y],
-                               [nose_x, nose_y],
-                               [left_mouth_x, left_mouth_y],
-                               [right_mouth_x, right_mouth_y]].
-        mean_landmarks  : Mean shape, should be normalized in [-0.5, 0.5].
-        crop_size       : Output image size.
-        face_factor     : The factor of face area relative to the output image.
-        landmark_factor : The factor of landmarks' area relative to the face.
-        align_type      : 'similarity' or 'affine'.
-        order           : The order of interpolation. The order has to be in the range 0-5:
-                              - 0: INTER_NEAREST
-                              - 1: INTER_LINEAR
-                              - 2: INTER_AREA
-                              - 3: INTER_CUBIC
-                              - 4: INTER_LANCZOS4
-                              - 5: INTER_LANCZOS4
-        mode            : One of ['constant', 'edge', 'symmetric', 'reflect', 'wrap'].
-                          Points outside the boundaries of the input are filled according
-                          to the given mode.
+        img                : Face image to be aligned and cropped.
+        src_landmarks      : [[x_1, y_1], ..., [x_n, y_n]].
+        standard_landmarks : Standard shape, should be normalized.
+        crop_size          : Output image size, should be 1. int for (crop_size, crop_size)
+                             or 2. (int, int) for (crop_size_h, crop_size_w).
+        face_factor        : The factor of face area relative to the output image.
+        align_type         : 'similarity' or 'affine'.
+        order              : The order of interpolation. The order has to be in the range 0-5:
+                                 - 0: INTER_NEAREST
+                                 - 1: INTER_LINEAR
+                                 - 2: INTER_AREA
+                                 - 3: INTER_CUBIC
+                                 - 4: INTER_LANCZOS4
+                                 - 5: INTER_LANCZOS4
+        mode               : One of ['constant', 'edge', 'symmetric', 'reflect', 'wrap'].
+                             Points outside the boundaries of the input are filled according
+                             to the given mode.
     """
     # set OpenCV
     import cv2
@@ -60,122 +43,59 @@ def align_crop_5pts_opencv(img,
     assert order in [0, 1, 2, 3, 4, 5], 'Invalid `order`! Allowed: %s!' % [0, 1, 2, 3, 4, 5]
     assert mode in ['constant', 'edge', 'symmetric', 'reflect', 'wrap'], 'Invalid `mode`! Allowed: %s!' % ['constant', 'edge', 'symmetric', 'reflect', 'wrap']
 
-    # move
-    move = np.array([img.shape[1] // 2, img.shape[0] // 2])
-
-    # pad border
-    v_border = img.shape[0] - crop_size
-    w_border = img.shape[1] - crop_size
-    if v_border < 0:
-        v_half = (-v_border + 1) // 2
-        img = np.pad(img, ((v_half, v_half), (0, 0), (0, 0)), mode=mode)
-        src_landmarks += np.array([0, v_half])
-        move += np.array([0, v_half])
-    if w_border < 0:
-        w_half = (-w_border + 1) // 2
-        img = np.pad(img, ((0, 0), (w_half, w_half), (0, 0)), mode=mode)
-        src_landmarks += np.array([w_half, 0])
-        move += np.array([w_half, 0])
+    # crop size
+    if isinstance(crop_size, (list, tuple)) and len(crop_size) == 2:
+        crop_size_h = crop_size[0]
+        crop_size_w = crop_size[1]
+    elif isinstance(crop_size, int):
+        crop_size_h = crop_size_w = crop_size
+    else:
+        raise Exception('Invalid `crop_size`! `crop_size` should be 1. int for (crop_size, crop_size) or 2. (int, int) for (crop_size_h, crop_size_w)!')
 
     # estimate transform matrix
-    mean_landmarks -= np.array([mean_landmarks[0, :] + mean_landmarks[1, :]]) / 2.0  # middle point of eyes as center
-    trg_landmarks = mean_landmarks * (crop_size * face_factor * landmark_factor) + move
+    trg_landmarks = standard_landmarks * max(crop_size_h, crop_size_w) * face_factor + np.array([crop_size_w // 2, crop_size_h // 2])
     if align_type == 'affine':
         tform = cv2.estimateAffine2D(trg_landmarks, src_landmarks, ransacReprojThreshold=np.Inf)[0]
     else:
         tform = cv2.estimateAffinePartial2D(trg_landmarks, src_landmarks, ransacReprojThreshold=np.Inf)[0]
 
-    # fix the translation to match the middle point of eyes
-    trg_mid = (trg_landmarks[0, :] + trg_landmarks[1, :]) / 2.0
-    src_mid = (src_landmarks[0, :] + src_landmarks[1, :]) / 2.0
-    new_trg_mid = cv2.transform(np.array([[trg_mid]]), tform)[0, 0]
-    tform[:, 2] += src_mid - new_trg_mid
-
     # warp image by given transform
-    output_shape = (crop_size // 2 + move[1] + 1, crop_size // 2 + move[0] + 1)
-    img_align = cv2.warpAffine(img, tform, output_shape[::-1], flags=cv2.WARP_INVERSE_MAP + inter[order], borderMode=border[mode])
+    output_shape = (crop_size_h, crop_size_w)
+    img_crop = cv2.warpAffine(img, tform, output_shape[::-1], flags=cv2.WARP_INVERSE_MAP + inter[order], borderMode=border[mode])
 
-    # crop
-    img_crop = img_align[-crop_size:, -crop_size:]
+    # get transformed landmarks
+    tformed_landmarks = cv2.transform(np.expand_dims(src_landmarks, axis=0), cv2.invertAffineTransform(tform))[0]
 
-    return img_crop
+    return img_crop, tformed_landmarks
 
 
-def align_crop_5pts_skimage(img,
-                            src_landmarks,
-                            mean_landmarks=_DEFAULT_MEAN_LANDMARKS,
-                            crop_size=512,
-                            face_factor=0.7,
-                            landmark_factor=0.35,
-                            align_type='similarity',
-                            order=3,
-                            mode='edge'):
-    """Align and crop a face image by 5 landmarks.
+def align_crop_skimage(img,
+                       src_landmarks,
+                       standard_landmarks,
+                       crop_size=512,
+                       face_factor=0.7,
+                       align_type='similarity',
+                       order=3,
+                       mode='edge'):
+    """Align and crop a face image by landmarks.
 
     Arguments:
-        img             : Face image to be aligned and cropped.
-        src_landmarks   : 5 landmarks:
-                              [[left_eye_x, left_eye_y],
-                               [right_eye_x, right_eye_y],
-                               [nose_x, nose_y],
-                               [left_mouth_x, left_mouth_y],
-                               [right_mouth_x, right_mouth_y]].
-        mean_landmarks  : Mean shape, should be normalized in [-0.5, 0.5].
-        crop_size       : Output image size.
-        face_factor     : The factor of face area relative to the output image.
-        landmark_factor : The factor of landmarks' area relative to the face.
-        align_type      : 'similarity' or 'affine'.
-        order           : The order of interpolation. The order has to be in the range 0-5:
-                              - 0: Nearest-neighbor
-                              - 1: Bi-linear
-                              - 2: Bi-quadratic
-                              - 3: Bi-cubic
-                              - 4: Bi-quartic
-                              - 5: Bi-quintic
-        mode            : One of ['constant', 'edge', 'symmetric', 'reflect', 'wrap'].
-                          Points outside the boundaries of the input are filled according
-                          to the given mode.
+        img                : Face image to be aligned and cropped.
+        src_landmarks      : [[x_1, y_1], ..., [x_n, y_n]].
+        standard_landmarks : Standard shape, should be normalized.
+        crop_size          : Output image size, should be 1. int for (crop_size, crop_size)
+                             or 2. (int, int) for (crop_size_h, crop_size_w).
+        face_factor        : The factor of face area relative to the output image.
+        align_type         : 'similarity' or 'affine'.
+        order              : The order of interpolation. The order has to be in the range 0-5:
+                                 - 0: INTER_NEAREST
+                                 - 1: INTER_LINEAR
+                                 - 2: INTER_AREA
+                                 - 3: INTER_CUBIC
+                                 - 4: INTER_LANCZOS4
+                                 - 5: INTER_LANCZOS4
+        mode               : One of ['constant', 'edge', 'symmetric', 'reflect', 'wrap'].
+                             Points outside the boundaries of the input are filled according
+                             to the given mode.
     """
-    import skimage.transform as transform
-
-    # check
-    assert align_type in ['affine', 'similarity'], 'Invalid `align_type`! Allowed: %s!' % ['affine', 'similarity']
-    assert order in [0, 1, 2, 3, 4, 5], 'Invalid `order`! Allowed: %s!' % [0, 1, 2, 3, 4, 5]
-    assert mode in ['constant', 'edge', 'symmetric', 'reflect', 'wrap'], 'Invalid `mode`! Allowed: %s!' % ['constant', 'edge', 'symmetric', 'reflect', 'wrap']
-
-    # move
-    move = np.array([img.shape[1] // 2, img.shape[0] // 2])
-
-    # pad border
-    v_border = img.shape[0] - crop_size
-    w_border = img.shape[1] - crop_size
-    if v_border < 0:
-        v_half = (-v_border + 1) // 2
-        img = np.pad(img, ((v_half, v_half), (0, 0), (0, 0)), mode=mode)
-        src_landmarks += np.array([0, v_half])
-        move += np.array([0, v_half])
-    if w_border < 0:
-        w_half = (-w_border + 1) // 2
-        img = np.pad(img, ((0, 0), (w_half, w_half), (0, 0)), mode=mode)
-        src_landmarks += np.array([w_half, 0])
-        move += np.array([w_half, 0])
-
-    # estimate transform matrix
-    mean_landmarks -= np.array([mean_landmarks[0, :] + mean_landmarks[1, :]]) / 2.0  # middle point of eyes as center
-    trg_landmarks = mean_landmarks * (crop_size * face_factor * landmark_factor) + move
-    tform = transform.estimate_transform(align_type, trg_landmarks, src_landmarks)
-
-    # fix the translation to match the middle point of eyes
-    trg_mid = (trg_landmarks[0, :] + trg_landmarks[1, :]) / 2.0
-    src_mid = (src_landmarks[0, :] + src_landmarks[1, :]) / 2.0
-    new_trg_mid = transform.matrix_transform([trg_mid], tform.params)[0]
-    tform.params[:2, 2] += src_mid - new_trg_mid
-
-    # warp image by given transform
-    output_shape = (crop_size // 2 + move[1] + 1, crop_size // 2 + move[0] + 1)
-    img_align = transform.warp(img, tform, output_shape=output_shape, order=order, mode=mode)
-
-    # crop
-    img_crop = img_align[-crop_size:, -crop_size:]
-
-    return img_crop
+    raise NotImplementedError("'align_crop_skimage' is not implemented!")
